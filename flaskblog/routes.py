6f9458ba06855e208a91a5cbe503a19c841e9fc2
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-16 02:42:53 +0000

    User Authentication

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -30,7 +30,7 @@ def about():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if current_user.isauthenticated:
+    if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -44,15 +44,15 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if current_user.isauthenticated:
+    if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            next
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template("login.html", title="Login", form=form)
